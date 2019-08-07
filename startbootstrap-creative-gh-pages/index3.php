9a81bebcb94707086a004d8d3ef5a82dc6f20ffe
hjk14985 <51595672+hjk14985@users.noreply.github.com>
2019-08-07 18:14:54 +0900

    Add files via upload

---
+++ startbootstrap-creative-gh-pages/index3.php
@@ -0,0 +1,396 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>GameOn</title>
+
+  <!-- Font Awesome Icons -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+
+  <!-- Plugin CSS -->
+  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+  <!-- Theme CSS - Includes Bootstrap -->
+  <link href="css/creative.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">GameOn</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto my-2 my-lg-0">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div id="demo" class="carousel slide" data-ride="carousel">
+
+    <!-- Indicators -->
+    <ul class="carousel-indicators">
+      <li data-target="#demo" data-slide-to="0" class="active"></li>
+      <li data-target="#demo" data-slide-to="1"></li>
+    </ul>
+
+    <!-- The slideshow -->
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/bg-masthead.jpg" alt="Los Angeles" width="100%" height="100%">
+      </div>
+      <div class="carousel-item">
+        <img src="img/bg-masthead2.jpg" alt="Chicago" width="100%" height="100%">
+      </div>
+    </div>
+    <div class="carousel text-white" >
+        GameOn
+    </div>
+    <!-- Left and right controls -->
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
+  </div>
+
+
+  <!-- Masthead -->
+  <!--
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end">
+          <h1 class=" text-white font-weight-bold">GameOn<span class="japanese_font">にようこそ</span></h1>
+          <hr class="divider my-4">
+        </div>
+        <div class="col-lg-8 align-self-baseline">
+          <p class="text-white-50 font-weight-light mb-5">GameOnではゲームのキャラクター,背景、プログラミング<br>を学べることができます。</p>
+          <a class="btn btn-primary btn-xl js-scroll-trigger" href="#services">もっと見る</a>
+        </div>
+      </div>
+    </div>
+  </header>
+-->
+
+  <!-- Services Section -->
+  <section class="page-section" id="services">
+    <div class="container">
+      <h2 class="text-center mt-0">どんなことをするのか。</h2>
+      <hr class="divider my-4">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-gem text-primary mb-4"></i>
+            <h3 class="h4 mb-2">3Dモデリング</h3>
+            <p class="text-muted mb-0">Max,Maya,Zbrushを使ってモデリングをするのを学べます。</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-laptop-code text-primary mb-4"></i>
+            <h3 class="h4 mb-2">2Dデザイン</h3>
+            <p class="text-muted mb-0">Potoshop,Illustを使ってあなたが望むな作品を作れます。</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-globe text-primary mb-4"></i>
+            <h3 class="h4 mb-2">プログラミング</h3>
+              <p class="text-muted mb-0">色んなプログラム言語を使ってゲームを作れます。</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-heart text-primary mb-4"></i>
+            <h3 class="h4 mb-2">ゲームエンジン</h3>
+            <p class="text-muted mb-0">Unity,Unreal Engine4を使ってゲームを作れます </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Portfolio Section -->
+  <section id="portfolio">
+    <div class="container-fluid p-0">
+      <div class="row no-gutters">
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/1.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/1.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Category
+              </div>
+              <div class="project-name">
+                Project Name
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/2.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/2.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Category
+              </div>
+              <div class="project-name">
+                Project Name
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/3.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/3.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Category
+              </div>
+              <div class="project-name">
+                Project Name
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/4.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/4.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Category
+              </div>
+              <div class="project-name">
+                Project Name
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/5.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/5.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Category
+              </div>
+              <div class="project-name">
+                Project Name
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/6.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/6.jpg" alt="">
+            <div class="portfolio-box-caption p-3">
+              <div class="project-category text-white-50">
+                Category
+              </div>
+              <div class="project-name">
+                Project Name
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Call to Action Section -->
+  <section class="text-white" style="background:url(/startbootstrap-creative-gh-pages/img/bg-masthead5.jpg); margin-top: 5rem; padding-top: 10rem; padding-bottom: 10rem; ">
+    <div class="container text-center ba">
+      <h2 class="mb-4">作品を見に行く。</h2>
+      <a class="btn btn-light btn-xl" href="https://www.artstation.com/artwork/Oyqonb">Artstation</a>
+    </div>
+  </section>
+
+
+  <?php
+      // define variables and set to empty values
+      $name = $email = $gender = $comment = $website = "";
+
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = test_input($_POST["name"]);
+        $email = test_input($_POST["email"]);
+        $website = test_input($_POST["website"]);
+        $comment = test_input($_POST["comment"]);
+        $gender = test_input($_POST["gender"]);
+      }
+
+      function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+
+      ?>
+
+    <section id="form" style="margin-top: 5rem; padding-top: 10rem; padding-bottom: 10rem; background:url(/startbootstrap-creative-gh-pages/img/bg-masthead3.jpg); color:white;">
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col-6 ml-auto">
+                <form action="/action_page.php">
+                  <div class="form-group">
+                    <label for="email">メールアドレス : </label>
+                    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                  </div>
+                  <div class="form-group">
+                    <label for="name">お名前 :  </label>
+                    <input type="text" class="form-control" id="name" placeholder="Enter name" name="name">
+                  </div>
+                  <div class="form-group">
+                    <label for="comment">コメント : </label>
+                    <input type="textarea" class="form-control" id="comment" placeholder="Enter comment" name="comment">
+                  </div>
+                  <div>
+                    <lable for="optradio">性別 : </lable>
+                    <div class="form-check-inline">
+                      <label class="form-check-label">
+                        <input type="radio" class="form-check-input" name="gender" value="female">女性
+                      </label>
+                    </div>
+                    <div class="form-check-inline">
+                      <label class="form-check-label">
+                        <input type="radio" class="form-check-input" name="gender" value="male">男性
+                      </label>
+                    </div>
+                    <div class="form-check-inline disabled">
+                      <label class="form-check-label">
+                        <input type="radio" class="form-check-input" name="gender" value="anthor">その他
+                      </label>
+                    </div>
+                  </div>
+                  <button type="submit" class="btn btn-primary">転送</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+
+<!-- table
+          <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <table class="align-right">
+               <thead class="text-center">
+                 <tr>
+                   <th colspan="3">興味がある方は入力してください。</th>
+                 </tr>
+               </thead>
+               <tbody>
+                 <tr>
+                   <td>お名前 : </td>
+                   <td><input type="text" name="name"></td>
+                 </tr>
+                 <tr>
+                   <td>メールアドレス : </td>
+                   <td><input type="text" name="email"></td>
+                 </tr>
+                 <tr>
+                   <td>コメント : </td>
+                   <td> <textarea name="comment" rows="2" cols="40"></textarea></td>
+                 </tr>
+                 <tr>
+                   <td>性別 : </td>
+                     <td class="text-center">         <input type="radio" name="gender" value="female">女性
+                                 <input type="radio" name="gender" value="male">男性
+                                 <input type="radio" name="gender" value="other">その他
+                    </td>
+                  <br><br>
+                   <td> <input type="submit" name="submit" value="転送"></td>
+                 </tr>
+               </tbody>
+             </table>
+          </form>
+    </section>
+
+-->
+
+  <!-- Contact Section -->
+  <section class="page-section" id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-4">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.3753308486657!2d139.72044131561216!3d35.69238033696721!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cf1ec610e2b%3A0x530894838b7b2b0f!2z7JWE7LyA67O064W467CU7IucIOyXrQ!5e0!3m2!1sko!2sjp!4v1565167677895!5m2!1sko!2sjp"
+           width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+        <div class="col-lg-8 text-center">
+          <h2 class="mt-0">一緒にゲームを作りませんか？</h2>
+          <hr class="divider my-4">
+          <p class="text-muted mb-5　width-500">ゲームを作ってみたいそしてプログラミングをして自分が好きなゲームを作りたくなったら連絡ください!</p>
+          <div class="row">
+            <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+              <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+              <div>+81 (03) 555-0149</div>
+            </div>
+            <div class="col-lg-4 mr-auto text-center">
+              <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+              <!-- Make sure to change the email address in anchor text AND the link below! -->
+              <a class="d-block" href="mailto:contact@yourwebsite.com">GameOn@GameOn.com</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- Footer -->
+  <footer class="bg-light py-5">
+    <div class="container">
+      <div class="small text-center text-muted">GameOn &copy; 2019 - InterOn</div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
+
+
+
+<?php
+    echo $name;
+    echo "<br>";
+    echo $email;
+    echo "<br>";
+    echo $comment;
+    echo "<br>";
+    echo $gender;
+?>
