e2a44ddaaf5afe46f7875a612d4ab4175be45be9
hjk14985 <51595672+hjk14985@users.noreply.github.com>
2019-10-13 19:47:27 +0900

    Add files via upload

---
+++ practice2/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="style.css">
+  <title>Game</title>
+</head>
+<body>
+  <div class="container">
+    <div class="menu">
+      <ul>
+        <li class="logo"><img src="diamond.png"></li>
+        <li class="active">Home</li>
+        <li>Services</li>
+        <li>Product</li>
+        <li>Contack</li>
+        <li><a href="#" class="sighup-btn"><span>Sign up</span></a></li>
+      </ul>
+    </div>
+    <div class="banner">
+      <div class="app-text">
+        <h1>Our besic thesis for bitcoin<br> is that it is better than gold.</h1>
+        <p>
+          Contrary to popular belief, Lorem ipsum dolor sit amet,<br> consectetur adipisicing elit.<br> Assumenda amet ipsam velit ex, vero magnam quae harum,<br> earum sunt sint atque iure impedit vitae voluptatibus quisquam,<br> voluptatum veniam molestias. Quibusdam.
+        </p>
+        <div class="play-btn">
+          <div class="play-btn-inner">
+            <i class="fa fa-play"></i>
+          </div>
+          <small><b>LEARN MORE</b></small>
+        </div>
+      </div>
+      <div class="app-picture">
+        <img src="Artboard.png">
+      </div>
+    </div>
+    <div class="quick-links">
+      <ul>
+        <li><i class="fa fa-share-alt"></i><p>SHARE TO</P></li>
+        <li><i class="fa fa-audio-description"></i><p>REENT NEWS</P></li>
+        <li><i class="fa fa-cog"></i><p>ANALYTICS</P></li>
+        <li><i class="fa fa-btc"></i><p>WALLET</P></li>
+      </ul>
+    </div>
+    <div class="social-icons">
+      <ul>
+        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+      </ul>
+    </div>
+  </div>
+</body>
+</html>
